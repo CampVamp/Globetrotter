8ddc4519fbf814ff8930a069e0eab0b942042fdd
Ajay <ajayramsaravanan4@gmail.com>
2025-03-15 21:22:25 +0530

    feat: Make Landing Page

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,3 +1,13 @@
 export default function Home() {
-  return <div>Hello World!</div>;
+  return (
+    <div className="w-full h-screen bg-[#4635B1] flex flex-col gap-6 items-center justify-center">
+      <div className="text-[#B771E5] text-9xl font-bold">GlobeTrotter</div>
+      <div className="text-[#FFFBCA] text-3xl font-bold">
+        YOUR ULTIMATE TRAVEL GUESSING GAME
+      </div>
+      <div className="bg-[#AEEA94] py-4 px-6 text-white rounded-2xl font-bold">
+        Play Now!
+      </div>
+    </div>
+  );
 }
