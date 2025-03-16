2ec3f24f133d08878a9182021883c5d6c59554fc
Ajay <ajayramsaravanan4@gmail.com>
2025-03-16 10:12:17 +0530

    feat: Add Earth for Landing

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,6 +1,8 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
-    <div className="w-full h-screen bg-[#4635B1] flex flex-col gap-6 items-center justify-center">
+    <div className="relative w-full h-screen bg-[#4635B1] flex flex-col gap-6 items-center justify-center overflow-clip">
       <div className="text-[#B771E5] text-9xl font-bold">GlobeTrotter</div>
       <div className="text-[#FFFBCA] text-3xl font-bold">
         YOUR ULTIMATE TRAVEL GUESSING GAME
@@ -8,6 +10,13 @@ export default function Home() {
       <div className="bg-[#AEEA94] py-4 px-6 text-white rounded-2xl font-bold">
         Play Now!
       </div>
+      <Image
+        className="absolute bottom-0 translate-y-[1000px] animate-spin-slow infinite duration-75"
+        alt="Earth"
+        src="/earth.svg"
+        width={1500}
+        height={1500}
+      />
     </div>
   );
 }
